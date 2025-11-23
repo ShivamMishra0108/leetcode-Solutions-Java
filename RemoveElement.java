f34ae3ef7d69fa7418bdd46082544780250e3455
Shivam Mishra <shivammishra6339@gmail.com>
2025-11-23 22:10:04 +0530

    Add files via upload
    Remove Element Leetode - 27

---
+++ RemoveElement.java
@@ -0,0 +1,19 @@
+package Array;
+
+class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+
+        int n = nums.length;
+        int k = 0;
+
+        for(int i =0; i<n; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+
+            }
+        }
+        return k;
+
+    }
+}
