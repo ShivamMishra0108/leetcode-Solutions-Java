614979e81b64f3ce748ca30ec7698df39eb03233
Shivam Mishra <shivammishra6339@gmail.com>
2025-11-23 22:11:20 +0530

    Add files via upload
    Find First and Last Position of Element in Sorted Array Leetcode -34

---
+++ FirstaAndLastPosition.java
@@ -0,0 +1,18 @@
+package Array;
+
+class FirstaAndLastPosition {
+    public int[] searchRange(int[] nums, int target) {
+
+        int n = nums.length;
+        int [] ans = {-1,-1};
+
+        for(int i=0; i<n; i++){
+            if(nums[i] == target){
+                if(ans[0] == -1)ans[0] = i;
+                ans[1] = i;
+            }
+        }
+        return ans;
+
+    }
+}
